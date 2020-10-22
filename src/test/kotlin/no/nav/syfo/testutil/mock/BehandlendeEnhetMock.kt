cba0ef7666780403a55bfa78c9211202735bec4c
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-22 11:01:05 +0200

    Move BehandlendeEnhetGenerator to BehandlendeEnhetMock

--- src/test/kotlin/no/nav/syfo/testutil/mock/BehandlendeEnhetMock.kt
+++ src/test/kotlin/no/nav/syfo/testutil/mock/BehandlendeEnhetMock.kt
@@ -12,15 +12,16 @@ import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.syfo.client.enhet.BehandlendeEnhet
+import no.nav.syfo.testutil.UserConstants
 import no.nav.syfo.testutil.UserConstants.ARBEIDSTAKER_FNR
 import no.nav.syfo.testutil.UserConstants.ARBEIDSTAKER_2_FNR
-import no.nav.syfo.testutil.generator.generateBehandlendeEnhet
 import no.nav.syfo.testutil.getRandomPort
 
 class BehandlendeEnhetMock {
     private val port = getRandomPort()
     val url = "http://localhost:$port"
-    val behandlendeEnhet = generateBehandlendeEnhet
+
+    val behandlendeEnhet = generateBehandlendeEnhet()
     val server = mockBehandlendeEnhetServer(port, behandlendeEnhet)
 
     private fun mockBehandlendeEnhetServer(
@@ -48,4 +49,10 @@ class BehandlendeEnhetMock {
             }
         }
     }
+
+    fun generateBehandlendeEnhet() =
+        BehandlendeEnhet(
+            enhetId = "1234",
+            navn = UserConstants.NAV_ENHET
+        )
 }
