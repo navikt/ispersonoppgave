d111a59537e2782616ec1806237665ebe5a0d84c
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-07-06 16:35:29 +0200

    Move API features into separate file

--- src/main/kotlin/no/nav/syfo/api/ApiModule.kt
+++ src/main/kotlin/no/nav/syfo/api/ApiModule.kt
@@ -1,48 +1,29 @@
 package no.nav.syfo.api
 
 import io.ktor.application.*
-import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.jackson.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.util.*
-import no.nav.syfo.*
 import no.nav.syfo.auth.isInvalidToken
 import no.nav.syfo.client.enhet.BehandlendeEnhetClient
 import no.nav.syfo.client.veiledertilgang.VeilederTilgangskontrollClient
 import no.nav.syfo.database.database
+import no.nav.syfo.env
 import no.nav.syfo.oversikthendelse.OversikthendelseProducer
 import no.nav.syfo.personoppgave.PersonOppgaveService
 import no.nav.syfo.personoppgave.api.registerVeilederPersonOppgaveApi
-import no.nav.syfo.util.*
-import java.util.*
+import no.nav.syfo.state
 
 @KtorExperimentalAPI
 fun Application.apiModule(
     behandlendeEnhetClient: BehandlendeEnhetClient,
     oversikthendelseProducer: OversikthendelseProducer
 ) {
-    install(ContentNegotiation) {
-        jackson(block = configureJacksonMapper())
-    }
-
-    install(CallId) {
-        retrieve { it.request.headers["X-Nav-CallId"] }
-        retrieve { it.request.headers[HttpHeaders.XCorrelationId] }
-        generate { UUID.randomUUID().toString() }
-        verify { callId: String -> callId.isNotEmpty() }
-        header(NAV_CALL_ID_HEADER)
-    }
-
-    install(StatusPages) {
-        exception<Throwable> { cause ->
-            call.respond(HttpStatusCode.InternalServerError, cause.message ?: "Unknown error")
-            log.error("Caught exception", cause, getCallId(), getConsumerId())
-            throw cause
-        }
-    }
+    installCallId()
+    installContentNegotiation()
+    installStatusPages()
 
     intercept(ApplicationCallPipeline.Call) {
         if (call.request.uri.contains(Regex("is_alive|is_ready|prometheus"))) {
--- src/test/kotlin/no/nav/syfo/testutil/mock/VeilederTilgangskontrollMock.kt
+++ src/test/kotlin/no/nav/syfo/testutil/mock/VeilederTilgangskontrollMock.kt
@@ -1,16 +1,12 @@
 package no.nav.syfo.testutil.mock
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.application.*
-import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import no.nav.syfo.api.installContentNegotiation
 import no.nav.syfo.client.veiledertilgang.Tilgang
 import no.nav.syfo.testutil.UserConstants.ARBEIDSTAKER_FNR
 import no.nav.syfo.testutil.getRandomPort
@@ -42,13 +38,7 @@ class VeilederTilgangskontrollMock {
             factory = Netty,
             port = port
         ) {
-            install(ContentNegotiation) {
-                jackson {
-                    registerKotlinModule()
-                    registerModule(JavaTimeModule())
-                    configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                }
-            }
+            installContentNegotiation()
             routing {
                 get("/syfo-tilgangskontroll/api/tilgang/bruker") {
                     if (call.parameters["fnr"] == ARBEIDSTAKER_FNR.value) {
