7fc3fd6ad4adc06bd7d599cf57bf627f1fbb8d4a
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-07-06 16:16:05 +0200

    Move OIDC into authentcation package

--- src/main/kotlin/no/nav/syfo/api/authentication/OIDC.kt
+++ src/main/kotlin/no/nav/syfo/api/authentication/OIDC.kt
@@ -1,4 +1,4 @@
-package no.nav.syfo.api
+package no.nav.syfo.api.authentication
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.SerializationFeature
