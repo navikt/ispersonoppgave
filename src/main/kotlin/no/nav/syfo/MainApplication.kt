62588efba48e6076e27ab93b215cca5d07ea4020
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-05-05 11:50:54 +0200

    Use distroless JDK17-image (#48)

--- src/main/kotlin/no/nav/syfo/MainApplication.kt
+++ src/main/kotlin/no/nav/syfo/MainApplication.kt
@@ -81,7 +81,7 @@ fun main() {
 
     applicationEngineEnvironment.monitor.subscribe(ApplicationStarted) { application ->
         applicationState.ready = true
-        application.environment.log.info("Application is ready")
+        application.environment.log.info("Application is ready, running Java VM ${Runtime.version()}")
         if (environment.toggleKafkaConsumerEnabled) {
             launchKafkaTasks(
                 applicationState = applicationState,
