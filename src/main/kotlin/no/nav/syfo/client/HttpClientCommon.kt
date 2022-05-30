6bafb0204a14d761e892c9ebc874dca6ecd411f6
geir-waagboe <geir.arne.waagbo@nav.no>
2022-05-30 09:57:54 +0200

    Use ktor retry

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -1,31 +1,38 @@
 package no.nav.syfo.client
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import no.nav.syfo.util.configure
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
 
-fun httpClientDefault() = HttpClient(CIO) {
+val commonConfig: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {
     install(ContentNegotiation) {
         jackson { configure() }
+    }
+    install(HttpRequestRetry) {
+        retryOnExceptionIf(2) { _, cause ->
+            cause !is ClientRequestException
+        }
+        constantDelay(500L)
     }
     expectSuccess = true
 }
 
 val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
-    install(ContentNegotiation) {
-        jackson { configure() }
-    }
-    expectSuccess = true
+    this.commonConfig()
     engine {
         customizeClient {
             setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
         }
     }
 }
+
+fun httpClientDefault() = HttpClient(CIO, commonConfig)
 
 fun httpClientProxy() = HttpClient(Apache, proxyConfig)
