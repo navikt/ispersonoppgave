e42cc2d72eaaf7fcdd557562debd1c2bf3cf00a7
June Henriksen <june.henriksen2@nav.no>
2022-09-08 11:01:28 +0200

    Test that we close oppgaver when receiving statusendringer on dialogmøte

--- src/test/kotlin/no/nav/syfo/dialogmotestatusendring/DialogmoteStatusendringServiceSpek.kt
+++ src/test/kotlin/no/nav/syfo/dialogmotestatusendring/DialogmoteStatusendringServiceSpek.kt
@@ -1,7 +1,6 @@
 package no.nav.syfo.dialogmotestatusendring
 
 import io.ktor.util.*
-import no.nav.syfo.dialogmotestatusendring.domain.DialogmoteStatusendring
 import no.nav.syfo.dialogmotestatusendring.domain.DialogmoteStatusendringType
 import no.nav.syfo.dialogmotesvar.domain.DialogmoteSvartype
 import no.nav.syfo.dialogmotesvar.domain.KDialogmotesvar
@@ -11,7 +10,6 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldNotBe
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.time.OffsetDateTime
 import java.util.*
 
 @InternalAPI
@@ -31,21 +29,15 @@ object DialogmoteStatusendringServiceSpek : Spek({
             database.connection.dropData()
         }
 
-        it("Finish personoppgave when dialogmote has a referat") {
+        it("Finish personoppgave when a dialogmote gets a referat") {
             database.connection.createPersonOppgave(
                 kDialogmotesvar = kDialogmotesvar,
                 type = PersonOppgaveType.DIALOGMOTESVAR,
             )
-            val dialogmoteStatusendring = DialogmoteStatusendring(
-                personIdent = UserConstants.ARBEIDSTAKER_FNR,
-                type = DialogmoteStatusendringType.FERDIGSTILT,
-                endringTidspunkt = OffsetDateTime.now(),
-                dialogmoteUuid = dialogmoteUuid,
-                veilederIdent = UserConstants.VEILEDER_IDENT,
-            )
+            val statusendring = getDialogmotestatusendring(DialogmoteStatusendringType.FERDIGSTILT, dialogmoteUuid)
 
             database.connection.use { connection ->
-                processDialogmoteStatusendring(connection, dialogmoteStatusendring)
+                processDialogmoteStatusendring(connection, statusendring)
                 connection.commit()
             }
 
@@ -56,27 +48,62 @@ object DialogmoteStatusendringServiceSpek : Spek({
             dialogmotesvaroppgave.behandletTidspunkt shouldNotBe null
         }
 
-        it("Finish personoppgave when dialogmote is changing place or time") {
+        it("Finish personoppgave when a dialogmote changes place or time") {
+            database.connection.createPersonOppgave(
+                kDialogmotesvar = kDialogmotesvar,
+                type = PersonOppgaveType.DIALOGMOTESVAR,
+            )
+            val statusendring = getDialogmotestatusendring(DialogmoteStatusendringType.NYTT_TID_STED, dialogmoteUuid)
+
+            database.connection.use { connection ->
+                processDialogmoteStatusendring(connection, statusendring)
+                connection.commit()
+            }
+
+            val oppgaver = database.connection.getPersonOppgaveList(UserConstants.ARBEIDSTAKER_FNR)
+            val dialogmotesvaroppgave = oppgaver[0]
+            oppgaver.size shouldBeEqualTo 1
+            dialogmotesvaroppgave.type shouldBeEqualTo PersonOppgaveType.DIALOGMOTESVAR.name
+            dialogmotesvaroppgave.behandletTidspunkt shouldNotBe null
         }
 
-        it("Finish personoppgave when dialogmote is cancelled") {
+        it("Finish personoppgave when a dialogmote is cancelled") {
+            database.connection.createPersonOppgave(
+                kDialogmotesvar = kDialogmotesvar,
+                type = PersonOppgaveType.DIALOGMOTESVAR,
+            )
+            val statusendring = getDialogmotestatusendring(DialogmoteStatusendringType.AVLYST, dialogmoteUuid)
+
+            database.connection.use { connection ->
+                processDialogmoteStatusendring(connection, statusendring)
+                connection.commit()
+            }
+
+            val oppgaver = database.connection.getPersonOppgaveList(UserConstants.ARBEIDSTAKER_FNR)
+            val dialogmotesvaroppgave = oppgaver[0]
+            oppgaver.size shouldBeEqualTo 1
+            dialogmotesvaroppgave.type shouldBeEqualTo PersonOppgaveType.DIALOGMOTESVAR.name
+            dialogmotesvaroppgave.behandletTidspunkt shouldNotBe null
         }
 
-        it("Do nothing with personoppgave when a dialogmote has been created") {
+        it("Do nothing with personoppgave when a dialogmote is created") {
+            val statusendring = getDialogmotestatusendring(DialogmoteStatusendringType.INNKALT, dialogmoteUuid)
+
+            database.connection.use { connection ->
+                processDialogmoteStatusendring(connection, statusendring)
+                connection.commit()
+            }
+
+            val oppgaver = database.connection.getPersonOppgaveList(UserConstants.ARBEIDSTAKER_FNR)
+            oppgaver.size shouldBeEqualTo 0
         }
 
         it("Do nothing if there is no personoppgave for dialogmøte") {
-            // The case if statusendring is referat/endring/avlysning, and no one has bothered to rsvp the innkalling
-            val dialogmoteStatusendring = DialogmoteStatusendring(
-                personIdent = UserConstants.ARBEIDSTAKER_FNR,
-                type = DialogmoteStatusendringType.FERDIGSTILT,
-                endringTidspunkt = OffsetDateTime.now(),
-                dialogmoteUuid = dialogmoteUuid,
-                veilederIdent = UserConstants.VEILEDER_IDENT,
-            )
+            // The case if statusendring is referat/endring/avlysning, and no negative responses to the innkalling
+            val statusendring = getDialogmotestatusendring(DialogmoteStatusendringType.FERDIGSTILT, dialogmoteUuid)
 
             database.connection.use { connection ->
-                processDialogmoteStatusendring(connection, dialogmoteStatusendring)
+                processDialogmoteStatusendring(connection, statusendring)
                 connection.commit()
             }
 
