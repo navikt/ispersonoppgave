44d84d82dabfcb5b5d3ff079fdf8a898820cff72
geir-waagboe <geir.arne.waagbo@nav.no>
2022-12-09 11:44:30 +0100

    oppfolgingsplanLPS-topic to aiven

--- src/test/kotlin/no/nav/syfo/testutil/KOppfolgingsplanLPSNAVGenerator.kt
+++ src/test/kotlin/no/nav/syfo/testutil/KOppfolgingsplanLPSNAVGenerator.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.testutil
 
-import no.nav.syfo.domain.PersonIdent
 import no.nav.syfo.personoppgave.oppfolgingsplanlps.kafka.KOppfolgingsplanLPS
 import no.nav.syfo.testutil.UserConstants.ARBEIDSTAKER_FNR
 import no.nav.syfo.testutil.UserConstants.VIRKSOMHETSNUMMER
