ce5256c10d8daef29ed5269cd091bb7c90daed2f
John Martin Lindseth <john.martin.lindseth@nav.no>
2022-12-12 14:58:49 +0100

    Store dialogmøtesvar from kafka
    Co-authored-by: June Henriksen <june.henriksen2@nav.no>

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,4 +1,5 @@
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
--- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
+-- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser; --
