592d9fca30fefce3e029ce388fe0a97f8276bf8e
June Henriksen <june.henriksen2@nav.no>
2022-12-12 15:35:45 +0100

    Grant access to dialogmote_statusendring table
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -2,4 +2,4 @@ REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
--- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser; --
+-- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
