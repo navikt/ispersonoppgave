0e2516a18b0c2f4ac4d9c9bc8d17701f79ca65ec
John Martin Lindseth <john.martin.lindseth@nav.no>
2023-01-26 09:16:08 +0100

    Close actual oppgave for dialogmotesvar sent after mote was ferdigstilt
    Use correct ID, confused uuid and referanse_uuid in previous PR.
    
    https://jira.adeo.no/browse/FAGSYSTEM-261022

---
+++ src/main/resources/db/migration/V4_6__close_oppgaver_where_behandler_corrected_answer_4_corrected.sql
@@ -0,0 +1,6 @@
+UPDATE person_oppgave
+SET behandlet_tidspunkt = now(),
+    behandlet_veileder_ident = 'X000000',
+    publish = true,
+    published_at = null
+WHERE referanse_uuid IN ('501239cd-734d-4507-b183-be2e17bf0820');
