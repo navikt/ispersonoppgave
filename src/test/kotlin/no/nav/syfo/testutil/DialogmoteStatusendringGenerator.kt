fabfa4663599f5941d49c1080f545b8eb8c98c37
Anders Rognstad <anders.rognstad@nav.no>
2023-06-29 10:08:07 +0200

    Lint fix

--- src/test/kotlin/no/nav/syfo/testutil/DialogmoteStatusendringGenerator.kt
+++ src/test/kotlin/no/nav/syfo/testutil/DialogmoteStatusendringGenerator.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.testutil
 
 import no.nav.syfo.dialogmotestatusendring.domain.DialogmoteStatusendring
 import no.nav.syfo.dialogmotestatusendring.domain.DialogmoteStatusendringType
+import no.nav.syfo.dialogmotestatusendring.domain.PDialogmoteStatusendring
 import java.time.OffsetDateTime
 import java.util.*
 
@@ -17,5 +18,23 @@ fun generateDialogmotestatusendring(
         endringTidspunkt = endringTidspunkt,
         dialogmoteUuid = uuid,
         veilederIdent = UserConstants.VEILEDER_IDENT,
+    )
+}
+
+fun generatePDialogmotestatusendring(
+    type: DialogmoteStatusendringType = DialogmoteStatusendringType.INNKALT,
+    uuid: UUID = UUID.randomUUID(),
+    endringTidspunkt: OffsetDateTime = OffsetDateTime.now(),
+): PDialogmoteStatusendring {
+    return PDialogmoteStatusendring(
+        id = 1,
+        uuid = UUID.randomUUID().toString(),
+        moteUuid = uuid.toString(),
+        arbeidstakerIdent = UserConstants.ARBEIDSTAKER_FNR.value,
+        veilederIdent = UserConstants.VEILEDER_IDENT,
+        type = type.name,
+        endringTidspunkt = endringTidspunkt,
+        createdAt = OffsetDateTime.now(),
+        updatedAt = OffsetDateTime.now(),
     )
 }
