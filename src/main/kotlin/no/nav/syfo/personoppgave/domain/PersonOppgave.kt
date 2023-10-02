141a755329df3ce5c24ed21636c1f80d53fe2172
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-10-02 09:26:46 +0200

    IS-1581: Personoppgavetype for vurder stans (#160)

--- src/main/kotlin/no/nav/syfo/personoppgave/domain/PersonOppgave.kt
+++ src/main/kotlin/no/nav/syfo/personoppgave/domain/PersonOppgave.kt
@@ -66,6 +66,10 @@ fun PersonOppgave.toHendelseType(): PersonoppgavehendelseType {
             if (isUBehandlet()) PersonoppgavehendelseType.BEHANDLERDIALOG_MELDING_AVVIST_MOTTATT
             else PersonoppgavehendelseType.BEHANDLERDIALOG_MELDING_AVVIST_BEHANDLET
         }
+        PersonOppgaveType.AKTIVITETSKRAV_VURDER_STANS -> {
+            if (isUBehandlet()) PersonoppgavehendelseType.AKTIVITETSKRAV_VURDER_STANS_MOTTATT
+            else PersonoppgavehendelseType.AKTIVITETSKRAV_VURDER_STANS_BEHANDLET
+        }
     }
 }
 
