f489e31f744b46557ce0f28fed874b4759b8dba0
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-10-05 10:20:36 +0200

    IS-1581: Tilpasse dto for forhåndsvarsel (#164)

--- src/main/kotlin/no/nav/syfo/aktivitetskrav/VurderStansService.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/VurderStansService.kt
@@ -1,8 +1,8 @@
 package no.nav.syfo.aktivitetskrav
 
 import no.nav.syfo.aktivitetskrav.domain.ExpiredVarsel
+import no.nav.syfo.aktivitetskrav.domain.VarselType
 import no.nav.syfo.database.DatabaseInterface
-import no.nav.syfo.domain.PersonIdent
 import no.nav.syfo.metric.COUNT_PERSONOPPGAVEHENDELSE_AKTIVITETSKRAV_EXPIRED_VARSEL_MOTTATT
 import no.nav.syfo.personoppgave.*
 import no.nav.syfo.personoppgave.domain.*
@@ -15,14 +15,16 @@ class VurderStansService(
     fun processAktivitetskravExpiredVarsel(expiredVarselList: List<ExpiredVarsel>) {
         database.connection.use { connection ->
             expiredVarselList.forEach { expiredVarsel ->
-                log.info("Received aktivitetskrav expired varsel with uuid=${expiredVarsel.uuid}")
-                connection.createPersonOppgave(
-                    referanseUuid = expiredVarsel.uuid,
-                    personIdent = PersonIdent(expiredVarsel.personIdent),
-                    personOppgaveType = PersonOppgaveType.AKTIVITETSKRAV_VURDER_STANS,
-                    publish = true, // cronjob will publish
-                )
-                COUNT_PERSONOPPGAVEHENDELSE_AKTIVITETSKRAV_EXPIRED_VARSEL_MOTTATT.increment()
+                log.info("Received aktivitetskrav expired varsel with uuid=${expiredVarsel.varselUuid} with type ${expiredVarsel.varselType}")
+                if (expiredVarsel.varselType == VarselType.FORHANDSVARSEL_STANS_AV_SYKEPENGER) {
+                    connection.createPersonOppgave(
+                        referanseUuid = expiredVarsel.varselUuid,
+                        personIdent = expiredVarsel.personIdent,
+                        personOppgaveType = PersonOppgaveType.AKTIVITETSKRAV_VURDER_STANS,
+                        publish = true, // cronjob will publish
+                    )
+                    COUNT_PERSONOPPGAVEHENDELSE_AKTIVITETSKRAV_EXPIRED_VARSEL_MOTTATT.increment()
+                }
             }
             connection.commit()
         }
--- src/main/kotlin/no/nav/syfo/aktivitetskrav/domain/ExpiredVarsel.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskrav/domain/ExpiredVarsel.kt
@@ -1,10 +1,18 @@
 package no.nav.syfo.aktivitetskrav.domain
 
+import no.nav.syfo.domain.PersonIdent
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class ExpiredVarsel(
-    val uuid: UUID,
-    val personIdent: String,
+    val varselUuid: UUID,
+    val createdAt: LocalDateTime,
+    val personIdent: PersonIdent,
+    val varselType: VarselType,
     val svarfrist: LocalDate,
 )
+
+enum class VarselType {
+    FORHANDSVARSEL_STANS_AV_SYKEPENGER
+}
