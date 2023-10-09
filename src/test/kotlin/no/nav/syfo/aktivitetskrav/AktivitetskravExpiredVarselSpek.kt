cefe8b9d22ba82e63e192a789e2b2db403ce4e3d
geir-waagboe <geir.arne.waagbo@nav.no>
2023-10-09 10:56:25 +0200

    IS-1581: Handle duplicate forhandsvarsel

--- src/test/kotlin/no/nav/syfo/aktivitetskrav/AktivitetskravExpiredVarselSpek.kt
+++ src/test/kotlin/no/nav/syfo/aktivitetskrav/AktivitetskravExpiredVarselSpek.kt
@@ -12,6 +12,8 @@ import no.nav.syfo.domain.PersonIdent
 import no.nav.syfo.personoppgave.domain.PersonOppgaveType
 import no.nav.syfo.personoppgave.domain.toPersonOppgave
 import no.nav.syfo.personoppgave.getPersonOppgaverByReferanseUuid
+import no.nav.syfo.personoppgave.getUbehandledePersonOppgaver
+import no.nav.syfo.personoppgave.updatePersonoppgaveSetBehandlet
 import no.nav.syfo.testutil.ExternalMockEnvironment
 import no.nav.syfo.testutil.UserConstants.ARBEIDSTAKER_FNR
 import no.nav.syfo.testutil.dropData
@@ -97,6 +99,46 @@ class AktivitetskravExpiredVarselSpek : Spek({
 
                 val personOppgaver = database.connection.getPersonOppgaverByReferanseUuid(
                     referanseUuid = expiredVarsel.varselUuid,
+                ).map { it.toPersonOppgave() }
+                personOppgaver.size shouldBeEqualTo 1
+                val personOppgave = personOppgaver.first()
+                personOppgave.publish shouldBeEqualTo true
+                personOppgave.type shouldBeEqualTo PersonOppgaveType.AKTIVITETSKRAV_VURDER_STANS
+                personOppgave.personIdent shouldBeEqualTo ARBEIDSTAKER_FNR
+            }
+            it("Consumes expired varsel creates personoppgave if existing is behandlet") {
+                val expiredVarsel = ExpiredVarsel(
+                    varselUuid = UUID.randomUUID(),
+                    createdAt = LocalDate.now().atStartOfDay(),
+                    personIdent = PersonIdent(ARBEIDSTAKER_FNR.value),
+                    varselType = VarselType.FORHANDSVARSEL_STANS_AV_SYKEPENGER,
+                    svarfrist = LocalDate.now(),
+                )
+                kafkaConsumer.mockPollConsumerRecords(
+                    recordValue = expiredVarsel,
+                    topic = topic,
+                )
+                aktivitetskravExpiredVarselConsumer.pollAndProcessRecords(
+                    kafkaConsumer = kafkaConsumer,
+                )
+                database.connection.use {
+                    val personOppgave = it.getPersonOppgaverByReferanseUuid(
+                        referanseUuid = expiredVarsel.varselUuid,
+                    ).map { it.toPersonOppgave() }.first()
+                    it.updatePersonoppgaveSetBehandlet(personOppgave)
+                    it.commit()
+                }
+                kafkaConsumer.mockPollConsumerRecords(
+                    recordValue = expiredVarsel.copy(),
+                    topic = topic,
+                )
+                aktivitetskravExpiredVarselConsumer.pollAndProcessRecords(
+                    kafkaConsumer = kafkaConsumer,
+                )
+
+                val personOppgaver = database.connection.getUbehandledePersonOppgaver(
+                    personIdent = PersonIdent(ARBEIDSTAKER_FNR.value),
+                    personOppgaveType = PersonOppgaveType.AKTIVITETSKRAV_VURDER_STANS,
                 ).map { it.toPersonOppgave() }
                 personOppgaver.size shouldBeEqualTo 1
                 val personOppgave = personOppgaver.first()
