0ad4a601d25f7cdd42c7c6358e04441fc7982a89
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-10-21 14:45:26 +0200

    IS-3480: Lagt på semikolon

--- src/main/resources/db/migration/V6_3__close_oppgave_where_avvist_melding_answered.sql
+++ src/main/resources/db/migration/V6_3__close_oppgave_where_avvist_melding_answered.sql
@@ -1,4 +1,4 @@
 UPDATE PERSON_OPPGAVE
 SET behandlet_tidspunkt = now(),
     behandlet_veileder_ident = 'X000000'
-WHERE uuid = '414d9410-e719-4bf3-8825-b5ece9ddbca5'
+WHERE uuid = '414d9410-e719-4bf3-8825-b5ece9ddbca5';
